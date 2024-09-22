9aba212312287128871ed7ff306946fda90952ee
MinhazCodes-R <151343957+MinhazCodes-R@users.noreply.github.com>
2024-09-22 15:30:06 -0400

    Add files via upload

--- OtherPages/Experience/index.js
+++ OtherPages/Experience/index.js
@@ -1,51 +1,57 @@
 
 var layoutboolean = true;
+this.document.querySelectorAll(".avater").forEach( avatarelem =>{
+    avatarelem.addEventListener('click', ()=>{
+        this.document.querySelectorAll("#phoneview").forEach(listelem =>{
+            listelem.classList.toggle('hidden');
+            listelem.classList.toggle('show');
+        })
 
-document.querySelector(".avater").addEventListener('click',()=>{
-    //naturaly hidden
-    document.querySelector("#phoneview").classList.toggle('hidden');
-    document.querySelector("#phoneview").classList.toggle('show');
+        this.document.querySelectorAll("#profilepicture").forEach(listelem =>{
+            listelem.classList.toggle('margclose');
+            listelem.classList.toggle('margshow');
+        })
 
-    document.querySelector("#profilepicture").classList.toggle('margclose');
-    document.querySelector("#profilepicture").classList.toggle('margshow');
-    //For top buttons
 
-    document.querySelector("#buttonsmade01").classList.toggle('margclose');
-    document.querySelector("#buttonsmade01").classList.toggle('margshow');
+        if (this.document.body.clientWidth > 450){
 
-    document.querySelector("#writtencontactinfo01").classList.toggle('writteninfoclose');
-    document.querySelector("#writtencontactinfo01").classList.toggle('writteninfoopen');
+            this.document.querySelectorAll("#buttonsmade01").forEach(listelem =>{
+                listelem.classList.toggle('margclose');
+                listelem.classList.toggle('margshow');
+            })
 
-    document.querySelector('#canvas1').classList.toggle('margclose01');
-    document.querySelector('#canvas1').classList.toggle('margshow01');
+            this.document.querySelectorAll("#writtencontactinfo01").forEach(listelem =>{
+                listelem.classList.toggle('writteninfoclose');
+                listelem.classList.toggle('writteninfoopen');
+            })
 
-    document.querySelectorAll('.boxtemplate').forEach(listelem =>{
-        listelem.classList.toggle('margclose01')
-    });
-    document.querySelectorAll('.boxtemplate').forEach(listelem =>{
-        listelem.classList.toggle('margshow01')
-    });
+            document.querySelector('#canvas1').classList.toggle('margclose01');
+            document.querySelector('#canvas1').classList.toggle('margshow01');
 
-    //boxtemplate
+            document.querySelectorAll('.boxtemplate').forEach(listelem =>{
+                listelem.classList.toggle('margclose01')
+            });
+            document.querySelectorAll('.boxtemplate').forEach(listelem =>{
+                listelem.classList.toggle('margshow01')
+            });
 
-    if (document.querySelector('#canvas1').classList.contains('margclose01')){
-        adjustmentx = 0;
-    }
-    else{
-        adjustmentx = 180;
-    }
+            //boxtemplate
 
-    
-    // if (layoutboolean) {
-    //     document.getElementById("overallstructure").style.gridTemplateColumns = "0px 1fr";
-    //     layoutboolean = false;
-    // }
-    // else{
-    //     document.getElementById("overallstructure").style.gridTemplateColumns = "280px 1fr";
-    //     layoutboolean = true;
-    // }
+            if (document.querySelector('#canvas1').classList.contains('margclose01')){
+                adjustmentx = 0;
+            }
+            else{
+                adjustmentx = 180;
+            }
+        }
 
+
+        
+
+
+    })
 })
+
 
 
 //Maze Solving Begins
